7d3de0cefd70936714f4de08125513b4e51f0c52
Unknown <seguragabriel@unbosque.edu.co>
2019-05-07 16:25:49 -0500

    WIP may 07

---
+++ Menu/menu.js
@@ -0,0 +1,57 @@
+//Banner top
+$('#banner').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/diplomado-banner-principal.jpg" alt="Diplomado imagen"/>');
+
+// banner modulo 1
+$('#banner-titulo').html('<div>Diplomado Virtual</div><div>Osteoporosis</div>');
+
+// Menu numero 1 -----
+//$('#menu').html('<div id="menuanchor-top"> <nav> <div class="menu-toggle"> <i class="fa fa-bars"> </i> Menú </div><ul> <li class="home"> <a href="#"> <i class="fa fa-home"> </i> </a> </li><li class="dropdown timeit" data-start="2018-07-27 00:00"> <a href=""> <span>0 </span> <br>Lineamientos </a> <ul class="dropdown-menu"> <li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8889">Objetivos de aprendizaje / Introducción </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8890">¿Cómo se define osteoporosis? / Epidemiología </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8891">¿Cuáles son los resultados clínicos posteriores a una fractura? / </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8892">¿La osteoporosis se considera una prioridad de salud en Colombia? </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8893">¿Cuál es la perspectiva de la carga económica asociada a la osteoporosis? </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8894">Bibliografía </a> </li><li role="presentation" class="divider"> </li><li> <a href="#">Evaluación </a> </li><li role="presentation" class="divider"> </li></ul> </li><li class="dropdown timeit" data-start="2018-08-27 00:00"> <a href=""> <span>1 </span> <br>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">pagina </a> </li><li role="presentation" class="divider"> </li></ul> </li><li class="dropdown timeit" data-start="2018-08-27 00:00"> <a href=""> <span>2 </span> <br>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">pagina </a> </li><li role="presentation" class="divider"> </li></ul> </li><li class="dropdown timeit" data-start="2018-08-27 00:00"> <a href=""> <span>3 </span> <br>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">pagina </a> </li><li role="presentation" class="divider"> </li></ul> </li><li class="dropdown timeit" data-start="2018-08-27 00:00"> <a href=""> <span>4 </span> <br>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">pagina </a> </li><li role="presentation" class="divider"> </li></ul> </li><li class="dropdown timeit" data-start="2018-08-27 00:00"> <a href=""> <span>5 </span> <br>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">pagina </a> </li><li role="presentation" class="divider"> </li></ul> </li><li class="dropdown timeit" data-start="2018-08-27 00:00"> <a href=""> <span>6 </span> <br>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">pagina </a> </li><li role="presentation" class="divider"> </li></ul> </li><li class="dropdown timeit" data-start="2018-08-27 00:00"> <a href=""> <span>7 </span> <br>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">pagina </a> </li><li role="presentation" class="divider"> </li></ul> </li></ul> </nav></div>');
+
+// Menu numero 2 -----
+
+// footer curso amgen #1
+$('#footer').html('<div class="span5"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/amgen/images/logos/logo-amgen.svg" alt="AMGEN" /></div><div class="span1"></div><div class="span5"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/amgen/images/logos/logo-UEB.svg" alt="Universidad El Bosque" /></div><div class="span10"><p>© Todos los derechos reservados</p></div>');
+
+// footer curso amgen #2
+$('#footer-2').html('<div class="span5"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/amgen/images/logos/logo-amgen.svg" alt="AMGEN" /></div><div class="span1"></div><div class="span5"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/amgen/images/logos/logo-UEB.svg" alt="Universidad El Bosque" /></div><div class="span10"><p>© Todos los derechos reservados</p></div>');
+
+
+//
+// barras progreso
+//
+// progreso 10%
+$('#progreso10').html('<div class="progress progress-striped active"><div class="bar" style="width: 10%;"></div></div>');
+// progreso 20%
+$('#progreso20').html('<div class="progress progress-striped active"><div class="bar" style="width: 20%;"></div></div>');
+// progreso 30%
+$('#progreso30').html('<div class="progress progress-striped active"><div class="bar" style="width: 30%;"></div></div>');
+// progreso 40%
+$('#progreso40').html('<div class="progress progress-striped active"><div class="bar" style="width: 40%;"></div></div>');
+// progreso 50%
+$('#progreso50').html('<div class="progress progress-striped active"><div class="bar" style="width: 50%;"></div></div>');
+// progreso 60%
+$('#progreso60').html('<div class="progress progress-striped active"><div class="bar" style="width: 60%;"></div></div>');
+// progreso 70%
+$('#progreso70').html('<div class="progress progress-striped active"><div class="bar" style="width: 70%;"></div></div>');
+// progreso 80%
+$('#progreso80').html('<div class="progress progress-striped active"><div class="bar" style="width: 80%;"></div></div>');
+// progreso 90%
+$('#progreso90').html('<div class="progress progress-striped active"><div class="bar" style="width: 90%;"></div></div>');
+// progreso 100%
+$('#progreso100').html('<div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div>');
+
+//
+// elementos modulo 0
+//
+// banner mod 0
+$('#banner-mod-0').html('<div class="span6 txt-banner"><h1>Diplomado<br><span>Expertos en Lípidos</span></h1></div>');
+
+
+//
+// elementos modulo 1
+
+// material mod 1
+$('#material-mod-1').html('<div> <h2>Documentos <span>básicos de consulta </span>  </h2></div><ul class="thumbnails"> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod1/pdf01.png"> <div class="caption"> <h3>Documento PDF </h3> <p>The Latin America regional audit - epidemiología, costos e impacto de la osteoporosis en 2012 </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8895" class="btn btn-primary">Ver Documento </a> </p></div></div></li><li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod1/pdf02.png"> <div class="caption"> <h3>Documento PDF </h3> <p>The prevalence of radiographic vertebral fractures in latin American countries: the Latin American Vertebral Osteoporosis Study (LAVOS) </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8896" class="btn btn-primary">Ver Documento </a> </p></div></div></li><li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod1/pdf03.png"> <div class="caption"> <h3>Documento PDF </h3> <p>Costos de fracturas en mujeres con osteoporosis en Colombia </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8897" class="btn btn-primary">Ver Documento </a> </p></div></div></li></ul><ul class="thumbnails"> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod1/pdf04.png"> <div class="caption"> <h3>Documento PDF </h3> <p>Broken bones, broken lives: A roadmap to solve the fragility fracture crisis in Europe </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8898" class="btn btn-primary">Ver Documento </a> </p></div></div></li><li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod1/pdf05.png"> <div class="caption"> <h3>Documento PDF </h3> <p>The burden of osteoporosis in four Latin American countries: Brazil, Mexico, Colombia, and Argentina </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8899" class="btn btn-primary">Ver Documento </a> </p></div></div></li><li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod1/pdf06.png"> <div class="caption"> <h3>Presentación PDF </h3> <p>Carga de la enfermedad para el Sistema de Salud </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8900" class="btn btn-primary">Ver Documento </a> </p></div></div></li></ul>');
+
+// material mod 2
+$('#material-mod-2').html('<div> <h2>Documentos <span>básicos de consulta </span>  </h2></div><ul class="thumbnails"> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod2/pdf01.png"> <div class="caption"> <h3>Documento PDF </h3> <p>Bone Quality - The Material and Structural Basis of Bone Strength and Fragility </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296899&chapterid=8907" class="btn btn-primary">Ver Documento </a> </p></div></div></li><li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod2/pdf02.png"> <div class="caption"> <h3>Documento PDF </h3> <p>New insights into osteoclastogenic signaling mechanisms </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296899&chapterid=8908" class="btn btn-primary">Ver Documento </a> </p></div></div></li><li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod2/pdf03.png"> <div class="caption"> <h3>Documento PDF </h3> <p>Osteoporosis and Bone Mass Disorders: From Gene Pathways to Treatments </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296899&chapterid=8909" class="btn btn-primary">Ver Documento </a> </p></div></div></li></ul><ul class="thumbnails"> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod2/pdf04.png"> <div class="caption"> <h3>Documento PDF </h3> <p>WNT signaling in bone homeostasis and disease: from human mutations to treatments</p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296899&chapterid=8910" class="btn btn-primary">Ver Documento </a> </p></div></div></li><li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/osteoporosis-amgen/images/mod2/pdf05.png"> <div class="caption"> <h3>Presentación PDF </h3> <p>Fisiopatología de la osteoporosis </p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=296499&chapterid=8899" class="btn btn-primary">Ver Documento </a> </p></div></div></li></ul>');
